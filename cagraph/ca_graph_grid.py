40000c9833386623d14c250eb3c4c86ed94cd963
Francisco José Rodríguez Bogado <frbogado@geotexan.com>
2014-06-18 12:35:37 +0200

    HBars now are drawn from x=0

--- cagraph/ca_graph_grid.py
+++ cagraph/ca_graph_grid.py
@@ -21,71 +21,75 @@
 import pygtk
 pygtk.require('2.0')
 
-import gtk, gtk.glade
+import gtk
+import gtk.glade
+
 
 class CaGridStyle:
+
     ''' grid stye class '''
-    
+
     def __init__(self):
         ''' set default style '''
         self.line_color = (0.3, 0.7, 0.3, 0.5)
         self.line_width = 0.5
-        
+
         self.zero_line_color = (0, 0.5, 0, 1.0)
         self.zero_line_width = 1.0
-        
+
         self.grid_level = 0
-        
+
+
 class CaGraphGrid():
+
     def __init__(self, main_graph, xaxis, yaxis):
         ''' set grid default parameters '''
         self.style = CaGridStyle()
-        
+
         self.graph = main_graph
         self.xaxis = main_graph.axiss[xaxis]
         self.yaxis = main_graph.axiss[yaxis]
-        
+
     def draw(self):
         ''' draw grid '''
         level = self.style.grid_level
         context = self.graph.context
         style = self.graph.graph_style
-        
+
         # draw grid lines
         context.set_source_rgba(*self.style.line_color)
         context.set_line_width(self.style.line_width)
-        
+
         # draw xaxis grid lines
         direction = 'bottom'
         d1 = 0
         d2 = style.height - style.margin - style.margin
         y_px = style.margin
-        
+
         self.xaxis.draw_tics(level, y_px, d1, d2, direction)
-        
+
         # draw yaxis grid lines
         direction = 'right'
         d1 = 0
         d2 = style.width - style.margin - style.margin
         x_px = style.margin
-        
+
         self.yaxis.draw_tics(level, x_px, d1, d2, direction)
-        
+
         # draw zero lines
         context.set_source_rgba(*self.style.zero_line_color)
         context.set_line_width(self.style.zero_line_width)
-        
-        if self.yaxis.min < 0 and  self.yaxis.max > 0:
+
+        if self.yaxis.min < 0 and self.yaxis.max > 0:
             y_px = self.yaxis.data_to_px(0)
-            
+
             context.move_to(style.margin, y_px)
             context.line_to(style.width - style.margin, y_px)
             context.stroke()
-            
-        if self.xaxis.min < 0 and  self.xaxis.max > 0:
+
+        if self.xaxis.min < 0 and self.xaxis.max > 0:
             x_px = self.xaxis.data_to_px(0)
-            
+
             context.move_to(x_px, style.margin)
             context.line_to(x_px, style.height - style.margin)
             context.stroke()
-            
--- cagraph/series/hbar.py
+++ cagraph/series/hbar.py
@@ -19,48 +19,51 @@
 # Author: Yaacov Zamir (2011) <kobi.zamir@gmail.com>
 
 from bar import CaGraphSeriesBar
-        
+
+
 class CaGraphSeriesHBar(CaGraphSeriesBar):
+
     def __init__(self, main_graph, xaxis, yaxis):
         ''' set bar default parameters '''
         CaGraphSeriesBar.__init__(self, main_graph, xaxis, yaxis)
-        
+
     def draw_bar(self):
         ''' draw bars '''
         data = self.data
-        
+
         if len(data) == 0:
             return
-        
+
         context = self.graph.context
         style = self.graph.graph_style
-        
+
         context.save()
-        
+
         # set a clip region for the expose event
         context.rectangle(style.margin, style.margin,
-            style.width - style.margin - style.margin, 
-            style.height - style.margin - style.margin)
+                          style.width - style.margin - style.margin,
+                          style.height - style.margin - style.margin)
         context.clip()
-        
+
         # draw lines
         context.set_line_width(self.style.line_width)
-        
+
         # draw bar
         height = self.style.bar_width
         for point in data:
             x_px = self.xaxis.data_to_px(point[0])
             y_px = self.yaxis.data_to_px(point[1])
-            zero_px = self.xaxis.data_to_px(1)
+            # zero_px = self.xaxis.data_to_px(1)
+            # Stupid bug of the day or it's me?
+            zero_px = self.xaxis.data_to_px(0)
             width = x_px - zero_px
-            
+
             context.set_source_rgba(*self.style.line_color)
             context.rectangle(zero_px, y_px - height / 2.0, width, height)
             context.stroke()
-            
+
             context.set_source_rgba(*self.style.fill_color)
             context.rectangle(zero_px, y_px - height / 2.0, width, height)
             context.fill()
-            
-        context.restore()
 
+        context.restore()
